0c166a576b4659b70b75fe37c88718a2dc741f73
floyrin <janedqwang@gmail.com>
2022-10-19 23:15:56 +1100

    so very true

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 // bot code time !
 // import * as dotenv from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js"
 import {
   messageAddMe,
   messageBalance,
   messageHi,
   messageWork,
-} from "./messages";
-import { saveMap, loadMap, userData } from "./data";
+} from "./messages"
+import { saveMap, loadMap, userData } from "./data"
+import { help } from "./sotrue"
 
-require("dotenv").config();
+require("dotenv").config()
 
 const client = new Client({
   intents: [
@@ -17,35 +18,38 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessages,
   ],
-});
+})
 ////////////////////////////////////////////////////////////////////////////////
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user!.tag}!`);
-  loadMap();
-});
+  console.log(`Logged in as ${client.user!.tag}!`)
+  loadMap()
+})
 
 client.on("messageCreate", (message) => {
   if (message.content.startsWith("!hi")) {
-    messageHi(message);
+    messageHi(message)
   }
 
   if (message.content.startsWith("!addme")) {
-    messageAddMe(message);
+    messageAddMe(message)
   }
 
   if (message.content.startsWith("!work")) {
-    messageWork(message);
+    messageWork(message)
   }
 
   if (message.content.startsWith("!balance")) {
-    messageBalance(message);
+    messageBalance(message)
   }
 
+  if (message.content.startsWith("!help")) {
+    help(message)
+  }
   // if (message.mentions.has("1031072718570922014")) {
   // }
-});
+})
 
 // client.on("interactionCreate", async (interaction) => {
 //   if (!interaction.isChatInputCommand()) return;
