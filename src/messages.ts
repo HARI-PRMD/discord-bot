71daad2ba41a97d5e5de48346f90d0d8fc94c7ac
HARI-PRMD <haripramodb@gmail.com>
2022-11-14 12:38:22 +1100

    added comments

--- src/messages.ts
+++ src/messages.ts
@@ -84,7 +84,7 @@ export function messageAddMe(message: Message): void {
       money: {
         balance: 0,
         lastTimeWorked: null,
-        perHour: 0
+        perHour: 0,
       },
     });
   } else {
@@ -115,43 +115,49 @@ export function messageWork(message: Message): void {
     );
     return;
   }
-    if (user.money.lastTimeWorked === null) {
-      // if its the first time the user is working, set the last
-      // time worked to current time
-      user.money.lastTimeWorked = Date.now();
-    }
-    
-    if (user.money.perHour > totalTimeWorkPerHour && differenceInMinutes(Date.now(), user.money.lastTimeWorked) < 60) {
-      // if the user has already worked more than the total times within the hour
-      // this tells them to try again later
-      const timeLeft: number = 60 - differenceInMinutes( Date.now(), user.money.lastTimeWorked);
-      message.reply(`You have already worked ${user.money.perHour} times this hour, please take care of yourself and try again in ${timeLeft} minutes!`)
-      console.log(
-        colors.inverse.brightYellow(" WORK ") +
-          colors.brightYellow(
-            `        ${message.author.username} tried to overwork.`
-          )
-      );
-      return;
-    }
+  if (user.money.lastTimeWorked === null) {
+    // if its the first time the user is working, set the last
+    // time worked to current time
+    user.money.lastTimeWorked = Date.now();
+  }
 
-    if (differenceInMinutes(Date.now(), user.money.lastTimeWorked) > 60) {
-      // if it has already been more than an hour
-      user.money.lastTimeWorked = Date.now();
-      user.money.perHour = 0;
-    }
-    
-    user.money.balance += wage;
-    user.money.perHour += 1;
+  if (
+    user.money.perHour >= totalTimeWorkPerHour &&
+    differenceInMinutes(Date.now(), user.money.lastTimeWorked) < 60
+  ) {
+    // if the user has already worked more than the total times within the hour
+    // this tells them to try again later
+    const timeLeft: number =
+      60 - differenceInMinutes(Date.now(), user.money.lastTimeWorked);
     message.reply(
-      `You coded at Jared's typing speed for 16 hours and now have $${user.money.balance}`
+      `You have already worked ${user.money.perHour} times this hour, please take care of yourself and try again in ${timeLeft} minutes!`
     );
     console.log(
-      colors.inverse.brightGreen(" WORK ") +
-        colors.brightGreen(
-          `        ${message.author.username} --> $${user.money.balance} : has worked ${user.money.perHour} times this hour`
+      colors.inverse.brightYellow(" WORK ") +
+        colors.brightYellow(
+          `        ${message.author.username} tried to overwork.`
         )
     );
+    return;
+  }
+
+  if (differenceInMinutes(Date.now(), user.money.lastTimeWorked) > 60) {
+    // if it has already been more than an hour
+    user.money.lastTimeWorked = Date.now();
+    user.money.perHour = 0;
+  }
+  // adds money and updates user.money details
+  user.money.balance += wage;
+  user.money.perHour += 1;
+  message.reply(
+    `You coded at Jared's typing speed for 16 hours and now have $${user.money.balance}`
+  );
+  console.log(
+    colors.inverse.brightGreen(" WORK ") +
+      colors.brightGreen(
+        `        ${message.author.username} --> $${user.money.balance} : has worked ${user.money.perHour} times this hour`
+      )
+  );
   saveMap();
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -183,9 +189,7 @@ export function messageRule(message: Message): void {
   const ruleEmbed = new EmbedBuilder()
     .setColor([229, 161, 162])
     .setTitle(`Our Lord Hehe only wants u to follow 1 rule 🥺💞`)
-    .setImage(
-      'attachment://rule.png'
-    )
+    .setImage("attachment://rule.png")
     .setTimestamp();
-  message.channel.send({ embeds: [ruleEmbed], files: ['./assets/rule.png'] });
+  message.channel.send({ embeds: [ruleEmbed], files: ["./assets/rule.png"] });
 }
