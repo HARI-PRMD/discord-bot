8ceb6449c4419951d6ff45213de03803a2aaf6b7
HARI-PRMD <haripramodb@gmail.com>
2022-12-04 22:46:04 +1100

    cleaned up index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,43 +27,6 @@ client.on("ready", () => {
   runOnStart();
 });
 
-// client.on("message", async (message: Message) => {
-//   if (!message.guild) return;
-//   if (message.author.bot) return;
-//   try {
-//     if (message.channel.id != "1031080328753840242") return;
-//     let response: string = await goodMorningConversation(message);
-//     message.reply(response);
-//   } catch (err) {
-//     throw new Error(`error occured: ${err}`);
-//   }
-// });
-
-// client.on("messageCreate", async (message: Message) => {
-//   if (!message.guild) return;
-//   if (message.author.bot) return;
-//   try {
-//     if (message.channel.id != message.channelId) return;
-//     let res = await goodMorningConversation(message);
-//     // let res = "Hi";
-//     console.log(message);
-//     message.reply(res);
-//   } catch {
-//     console.log(`Bot error, please try again!`, message);
-//   }
-// });
-
-// client.on("messageCreate", async (message: Message) => {
-//   if (message.author.bot) return;
-//   // if (message.channel.isDMBased()) {
-//   //   await DMfunctions(message);
-//   //   return;
-//   // }
-//   if (!message.channel.isDMBased()) {
-//     await guildFunctions(message);
-//     return;
-//   }
-// });
 client.on("messageCreate", async (message: Message) => {
   if (message.author.bot) return;
   if (message.channel.isDMBased()) {
