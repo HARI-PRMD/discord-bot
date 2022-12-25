18aa47e39ed2c27b618019c619b5fcbe01dcd6c6
HARI-PRMD <haripramodb@gmail.com>
2022-12-25 23:52:31 +1100

    created setup script for python sub program

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# run as `source setup.sh`
+(
+  cd src/hehe-chan-ai/
+  python3 -m venv bot
+  . bot/bin/activate
+  pip install nltk
+  pip install numpy
+  pip install torch
+  python3 train.py
+)
+activate () {
+  . src/hehe-chan-ai/bot/bin/activate
+}
+activate
+
+
